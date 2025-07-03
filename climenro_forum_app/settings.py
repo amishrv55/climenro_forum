cd643fe06a6c4eb02bd84333bde22e7a4c7eaccc
AMISH JAIN <amishrv55@gmail.com>
2025-07-03 22:45:53 +0530

    new setting changes

--- climenro_forum_app/settings.py
+++ climenro_forum_app/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2^n3970945t$4j^&qqp-2jzuzw-bapdskzq%)o0%p0igpq*hvi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = []
 
 
 # Application definition
